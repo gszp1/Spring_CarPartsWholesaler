5cca0d28badcdb16b5930d5203ef38a1d431008a
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 12:32:15 +0100

    Adds OrderDetails entity.

---
+++ src/main/java/org/example/zadanie1/model/OrderDetails.java
@@ -0,0 +1,26 @@
+package org.example.zadanie1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_details")
+public class OrderDetails {
+    
+    private Long quantity;
+
+    private BigDecimal unitPrice;
+
+}
