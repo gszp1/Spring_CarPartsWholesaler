4b7ad43a061e1a7a59be575302187a55f064db64
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 02:34:37 +0100

    Adds Category entity.

---
+++ src/main/java/org/example/zadanie1/model/Category.java
@@ -0,0 +1,22 @@
+package org.example.zadanie1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+}
