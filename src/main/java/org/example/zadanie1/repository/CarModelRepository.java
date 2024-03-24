011293de1d55af66f7c4b822d96f20d8a4e70f97
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 03:04:57 +0100

    Adds CarModelRepository interface.

---
+++ src/main/java/org/example/zadanie1/repository/CarModelRepository.java
@@ -0,0 +1,6 @@
+package org.example.zadanie1.repository;
+
+import org.example.zadanie1.model.CarModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarModelRepository extends CrudRepository<CarModel, Long> { }
