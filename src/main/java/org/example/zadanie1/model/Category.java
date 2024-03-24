59fd7bd97b3b6376f88069d34a20fd59a0d786e8
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 16:03:54 +0100

    Changes generationType of Category and Part id to IDENTITY.

--- src/main/java/org/example/zadanie1/model/Category.java
+++ src/main/java/org/example/zadanie1/model/Category.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToMany(mappedBy = "category")
