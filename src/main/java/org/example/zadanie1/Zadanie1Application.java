aee9ec78271290cf1dde289072bbd47dfaa090e3
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 02:26:04 +0100

    Initial commit.

---
+++ src/main/java/org/example/zadanie1/Zadanie1Application.java
@@ -0,0 +1,13 @@
+package org.example.zadanie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie1Application.class, args);
+	}
+
+}
