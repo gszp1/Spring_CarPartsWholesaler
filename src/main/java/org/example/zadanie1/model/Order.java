434591c4c979fffcd1a1056a261c6e811b9bc703
gszp1 <grzybekmateusz@poczta.fm>
2024-03-24 03:20:50 +0100

    Adds Order entity.

---
+++ src/main/java/org/example/zadanie1/model/Order.java
@@ -0,0 +1,30 @@
+package org.example.zadanie1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date placingDate;
+
+    private Date deliveryDate;
+
+    @Column(nullable = false)
+    private Integer status;
+}
