446a9d92f814a95c086c8ef73c021a435c71e9e4
gszp1 <grzybekmateusz@poczta.fm>
2024-03-25 11:01:59 +0100

    Adds RepositoriesTests class.

---
+++ src/test/java/org/example/zadanie1/RepositoriesTests.java
@@ -0,0 +1,57 @@
+package org.example.zadanie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepositoriesTests {
+
+    @Test
+    public void countingTransactionsTest() {
+        // Given:
+
+        // When:
+
+        // Then:
+
+    }
+
+    @Test
+    public void productTransactionsCountTest() {
+        // Given:
+
+        // When:
+
+        // Then:
+
+    }
+
+    @Test
+    public void userTransactionsCountTest() {
+        // Given:
+
+        // When:
+
+        // Then:
+    }
+
+    @Test
+    public void getUserByEmailTest() {
+        // Given:
+
+        // When:
+
+        // Then:
+    }
+
+
+    @Test
+    public void getProductsByPriceRangeTest() {
+        // Given:
+
+        // When:
+
+        // Then:
+
+    }
+}
