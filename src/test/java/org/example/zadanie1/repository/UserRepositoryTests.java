7fb21e78698cace6b9a8d7bb0be3010c4ed12905
gszp1 <grzybekmateusz@poczta.fm>
2024-03-26 23:03:24 +0100

    Adds getUserByEmailTest() definition.

--- src/test/java/org/example/zadanie1/repository/UserRepositoryTests.java
+++ src/test/java/org/example/zadanie1/repository/UserRepositoryTests.java
@@ -1,58 +1,59 @@
 package org.example.zadanie1.repository;
 
+import jakarta.transaction.Transactional;
 import org.example.zadanie1.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
+@SpringBootTest
+@Transactional
 public class UserRepositoryTests {
 
-    UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    private TestEntityManager entityManager;
-
+    @AfterEach
+    public void cleanTable() {
+        userRepository.deleteAll();
+    }
 
     @Test
     public void getUserByEmailTest() {
         // Populate table with example users
-//        User user1 = new User(
-//                "SteveBlum1",
-//                "Steve",
-//                "Blum",
-//                "SteveBlum@gmail.com",
-//                "111222333"
-//        );
-//        entityManager.persist(user1);
-//        User user = new User(
-//                "JohnTrevolta1",
-//                "John",
-//                "Trevolta",
-//                "JohnTrevolta@gmail.com",
-//                "222333444"
-//        );
-//        entityManager.persist(user);
-//        user = new User(
-//                "TomH1",
-//                "Tom",
-//                "Hanks",
-//                "TomHanks@gmail.com",
-//                "555666777"
-//        );
-//        entityManager.persist(user);
-//        // Retrieve user from database
-//        Optional<User> retrievedUser = userRepository.findByEmail(user1.getEmail());
-//        assertTrue(retrievedUser.isPresent());
-//        assertEquals(user1.getEmail(), retrievedUser.get().getEmail());
-        assert(true);
+        User user1 = new User(
+                "SteveBlum1",
+                "Steve",
+                "Blum",
+                "SteveBlum@gmail.com",
+                "111222333"
+        );
+        userRepository.save(user1);
+        User user = new User(
+                "JohnTrevolta1",
+                "John",
+                "Trevolta",
+                "JohnTrevolta@gmail.com",
+                "222333444"
+        );
+        userRepository.save(user);
+        user = new User(
+                "TomH1",
+                "Tom",
+                "Hanks",
+                "TomHanks@gmail.com",
+                "555666777"
+        );
+        userRepository.save(user);
+        // Retrieve user from database
+        Optional<User> retrievedUser = userRepository.findByEmail(user1.getEmail());
+        assertTrue(retrievedUser.isPresent());
+        assertEquals(user1.getEmail(), retrievedUser.get().getEmail());
     }
 }
