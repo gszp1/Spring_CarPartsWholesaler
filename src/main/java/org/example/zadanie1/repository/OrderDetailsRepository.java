0c4d32a83a44db86c33e937dca6019445ab0cdd3
gszp1 <grzybekmateusz@poczta.fm>
2024-03-27 01:30:41 +0100

    Adds partOrdersCountTest() implementation.

--- src/main/java/org/example/zadanie1/repository/OrderDetailsRepository.java
+++ src/main/java/org/example/zadanie1/repository/OrderDetailsRepository.java
@@ -3,4 +3,7 @@ package org.example.zadanie1.repository;
 import org.example.zadanie1.model.OrderDetails;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderDetailsRepository extends CrudRepository<OrderDetails, Long> { }
+public interface OrderDetailsRepository extends CrudRepository<OrderDetails, Long> {
+
+    public Long countByPartName(String name);
+}
