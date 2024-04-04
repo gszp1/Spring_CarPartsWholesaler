2fef49a4bb141cda0b49e325a7ee6cb6b8851211
gszp1 <grzybekmateusz@poczta.fm>
2024-04-04 21:46:11 +0200

    Changes Key type in OrderDetailsRepository | Adjust name of CarModelRepository.

--- src/main/java/org/example/zadanie1/repository/ModelRepository.java
+++ src/main/java/org/example/zadanie1/repository/ModelRepository.java
@@ -3,4 +3,4 @@ package org.example.zadanie1.repository;
 import org.example.zadanie1.model.Model;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CarModelRepository extends CrudRepository<Model, Long> { }
+public interface ModelRepository extends CrudRepository<Model, Long> { }
