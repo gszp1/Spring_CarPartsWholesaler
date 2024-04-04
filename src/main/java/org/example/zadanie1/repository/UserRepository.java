d2278bb6d5e878654504db40bb5d0c2d7d98603e
gszp1 <grzybekmateusz@poczta.fm>
2024-04-04 21:12:03 +0200

    Removes redundant imports.

--- src/main/java/org/example/zadanie1/repository/UserRepository.java
+++ src/main/java/org/example/zadanie1/repository/UserRepository.java
@@ -3,7 +3,6 @@ package org.example.zadanie1.repository;
 import org.example.zadanie1.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
