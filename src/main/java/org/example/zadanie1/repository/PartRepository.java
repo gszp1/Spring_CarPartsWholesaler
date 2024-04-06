718c6043432c227419a34d53f72e6c7aaa8aa593
gszp1 <grzybekmateusz@poczta.fm>
2024-04-06 13:59:15 +0200

    Adds populating tables before each test in OrderDetailsRepositoryTests.

--- src/main/java/org/example/zadanie1/repository/PartRepository.java
+++ src/main/java/org/example/zadanie1/repository/PartRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface PartRepository extends CrudRepository<Part, Long> {
 
     List<Part> findByUnitPriceBetween(BigDecimal startPrice, BigDecimal endPrice);
+
+    Optional<Part> findFirst();
 }
--- src/test/java/org/example/zadanie1/repository/OrderDetailsRepositoryTests.java
+++ src/test/java/org/example/zadanie1/repository/OrderDetailsRepositoryTests.java
@@ -1,21 +1,18 @@
 package org.example.zadanie1.repository;
 
 import jakarta.transaction.Transactional;
-import org.example.zadanie1.compositekey.OrderDetailsKey;
 import org.example.zadanie1.model.Order;
 import org.example.zadanie1.model.OrderDetails;
 import org.example.zadanie1.model.Part;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -30,44 +27,53 @@ public class OrderDetailsRepositoryTests {
     @Autowired
     private OrderRepository orderRepository;
 
-    @AfterEach
-    public void cleanTables() {
-        partRepository.deleteAll();
-        orderDetailsRepository.deleteAll();
-        orderRepository.deleteAll();
+    @BeforeEach
+    public void populateTables() {
+        populatePartsTable();
+        populateOrdersTable();
+        populateOrderDetailsTable();
     }
 
-    @Test
-    public void partOrdersCountTest() {
-        // Insert into database example parts
-        List<Part> parts = new ArrayList<>();
-        parts.add(new Part("Oil Filter", new BigDecimal("22.5"), 10L));
-        parts.add(new Part("Air Filter", new BigDecimal("10.5"), 40L));
-        parts.add(new Part("Seat", new BigDecimal("100.25"), 5L));
-        partRepository.saveAll(parts);
-
-        // Create Orders
-        List<Order> orders = new ArrayList<>();
-        Order order = new Order(LocalDate.now(), LocalDate.now().plusDays(1), 1);
-        orderRepository.save(order);
-        Order order2 = new Order(LocalDate.now(), LocalDate.now().plusDays(2), 1);
-        orderRepository.save(order2);
-
-        // Create Order detail
-        OrderDetails orderDetails = new OrderDetails(new OrderDetailsKey(parts.getFirst().getPartId(), order.getOrderId()),
-                parts.getFirst(),
-                order,
-                3L,
-                parts.getFirst().getUnitPrice()
+    private void populatePartsTable() {
+        partRepository.saveAll(Arrays.asList(
+                        new Part("Oil Filter", new BigDecimal("22.5"), 10L),
+                        new Part("Air Filter", new BigDecimal("10.5"), 40L),
+                        new Part("Seat", new BigDecimal("100.25"), 5L)
+                )
         );
-        orderDetailsRepository.save(orderDetails);
-        OrderDetails orderDetails2 = new OrderDetails(new OrderDetailsKey(parts.getFirst().getPartId(), order2.getOrderId()),
-                parts.getFirst(),
-                order2,
-                3L,
-                parts.getFirst().getUnitPrice()
+    }
+
+    private void populateOrdersTable() {
+        orderRepository.saveAll(Arrays.asList(
+                        new Order(LocalDate.now(), LocalDate.now().plusDays(1), 1),
+                        new Order(LocalDate.now(), LocalDate.now().plusDays(2), 1)
+                )
         );
-        orderDetailsRepository.save(orderDetails2);
-        assertEquals(2, orderDetailsRepository.countByPartName(parts.getFirst().getName()));
+    }
+
+    private void populateOrderDetailsTable() {
+        Optional<Order> firstOrder = orderRepository.findFirst();
+        if (firstOrder.isEmpty()) {
+            return;
+        }
+        Optional<Part> firstPart = partRepository.findFirst();
+        if (firstPart.isEmpty()) {
+            return;
+        }
+        orderDetailsRepository.save(new OrderDetails(
+                        firstPart.get(),
+                        firstOrder.get(),
+                        1L,
+                        firstPart.get().getUnitPrice()
+                )
+        );
+    }
+
+
+    @AfterEach
+    public void cleanTables() {
+        orderDetailsRepository.deleteAll();
+        partRepository.deleteAll();
+        orderRepository.deleteAll();
     }
 }
